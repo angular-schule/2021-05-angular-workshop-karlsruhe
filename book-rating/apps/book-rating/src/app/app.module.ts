e699b4ab23250f963f6504053781120b086416b7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-15 16:28:13 +0200

    Komponenten in Lib verschoben

--- book-rating/apps/book-rating/src/app/app.module.ts
+++ book-rating/apps/book-rating/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookComponent } from './book/book.component';
+import { FeatureBooksModule } from '@book-rating/feature-books';
 
 const appRoutes: Routes = [];
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, BookComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabled' }),
+    FeatureBooksModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- book-rating/libs/feature-books/src/lib/feature-books.module.ts
+++ book-rating/libs/feature-books/src/lib/feature-books.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookComponent } from './book/book.component';
 
 export const featureBooksRoutes: Route[] = [];
 
 @NgModule({
+  declarations: [DashboardComponent, BookComponent],
   imports: [CommonModule, RouterModule],
+  exports: [DashboardComponent]
 })
 export class FeatureBooksModule {}
