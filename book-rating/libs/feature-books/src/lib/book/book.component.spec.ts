d3f26604f553e4a829ef1a368aeb7ab1fe0ec3e1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-15 19:20:00 +0200

    nx test feature-books

--- book-rating/libs/feature-books/src/lib/book/book.component.spec.ts
+++ book-rating/libs/feature-books/src/lib/book/book.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BookComponent } from './book.component';
 
@@ -8,7 +9,8 @@ describe('BookComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
--- book-rating/libs/feature-books/src/lib/dashboard/dashboard.component.spec.ts
+++ book-rating/libs/feature-books/src/lib/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookStoreService } from '@book-rating/data-books';
+import { of } from 'rxjs';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -7,8 +9,16 @@ describe('DashboardComponent', () => {
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bsMock = {
+      getAll: jest.fn().mockReturnValue(of([]))
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        { provide: BookStoreService, useValue: bsMock }
+      ]
     })
     .compileComponents();
   });
