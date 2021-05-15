4b819c365fac717fbd8048d38606e1814ad20e1d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-15 16:31:57 +0200

    nx g c book-details --project=feature-books

--- book-rating/libs/feature-books/src/lib/feature-books.module.ts
+++ book-rating/libs/feature-books/src/lib/feature-books.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 export const featureBooksRoutes: Route[] = [];
 
 @NgModule({
-  declarations: [DashboardComponent, BookComponent],
+  declarations: [DashboardComponent, BookComponent, BookDetailsComponent],
   imports: [CommonModule, RouterModule],
   exports: [DashboardComponent]
 })
