042c5dce3f80a533aca4000521a1806f032d9b5e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-21 14:33:47 +0200

    first TDD!

--- book-rating/libs/data-books/src/lib/book-rating.service.spec.ts
+++ book-rating/libs/data-books/src/lib/book-rating.service.spec.ts
@@ -1,16 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { Book } from './book';
 
 import { BookRatingService } from './book-rating.service';
 
 describe('BookRatingService', () => {
   let service: BookRatingService;
+  let book: Book;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BookRatingService);
+    service = new BookRatingService();
+    book = {
+      isbn: '',
+      title: '',
+      rating: 3,
+      description: '',
+    }
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should rate up a book by one', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
   });
+
+  it('it should rate down by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('it should not rate higher than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('it should not rate lower than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('it should respect immutability and return a new book', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook).not.toBe(book);
+
+    const ratedBook2 = service.rateDown(book);
+    expect(ratedBook2).not.toBe(book);
+  })
 });
--- book-rating/libs/data-books/src/lib/book-rating.service.ts
+++ book-rating/libs/data-books/src/lib/book-rating.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Book } from '..';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookRatingService {
 
-  constructor() { }
+  readonly minRating = 1;
+  readonly maxRating = 5;
 
-  
+  rateDown(book: Book): Book {
+    return {
+      ...book,
+      rating: (book.rating > this.minRating) ? book.rating - 1 :  this.minRating
+    };
+  }
+
+  rateUp(book: Book): Book {
+    const rating = Math.min(book.rating + 1, this.maxRating);
+    return { ...book, rating };
+  }
 }
+
