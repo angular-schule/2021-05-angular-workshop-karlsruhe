b323ec6351c1d5798c90bd7f38d2410a58b364eb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-21 10:37:21 +0200

    final Web Components example!

--- book-rating/libs/feature-books/src/lib/feature-books.module.ts
+++ book-rating/libs/feature-books/src/lib/feature-books.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,6 +9,7 @@ export const featureBooksRoutes: Route[] = [];
 @NgModule({
   declarations: [DashboardComponent, BookComponent],
   imports: [CommonModule, RouterModule],
-  exports: [DashboardComponent]
+  exports: [DashboardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // <-- deaktiviert Fehlerprüfung bei unbekannten Elementen
 })
 export class FeatureBooksModule {}
