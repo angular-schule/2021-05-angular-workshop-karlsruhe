5b0f336d8d90206c3d4a9ab94ece393d7e037706
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-21 11:40:39 +0200

    nx: defining constraints and imports

--- book-rating/libs/feature-books/src/lib/feature-books.module.ts
+++ book-rating/libs/feature-books/src/lib/feature-books.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { DataBooksModule } from '@book-rating/data-books';
 
 export const featureBooksRoutes: Route[] = [];
 
 @NgModule({
   declarations: [DashboardComponent, BookComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    DataBooksModule
+  ],
   exports: [DashboardComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // <-- deaktiviert Fehlerprüfung bei unbekannten Elementen
 })
