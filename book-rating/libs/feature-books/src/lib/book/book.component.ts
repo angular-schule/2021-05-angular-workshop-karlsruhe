95282dabaf602a697bd6523f23a1063df80596d9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-21 09:54:15 +0200

    repeating stars!

--- book-rating/libs/feature-books/src/lib/book/book.component.ts
+++ book-rating/libs/feature-books/src/lib/book/book.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'books-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent  {
 
   @Input()
-  book: Book | undefined;
+  book?: Book;
+
+  get stars() {
+    return new Array(this.book?.rating || 0);
+  }
 }
