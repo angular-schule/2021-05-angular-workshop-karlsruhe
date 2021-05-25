0bc017279a477ea530ddc29d58c99cd085fdb5ce
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-25 10:07:08 +0200

    book-component: unit test against DOM

--- book-rating/apps/book-rating/src/app/app.component.spec.ts
+++ book-rating/apps/book-rating/src/app/app.component.spec.ts
@@ -1,18 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [
+        AppComponent,
+        // DashboardComponent, BookComponent --> Integrationstest
+      ],
+      schemas: [NO_ERRORS_SCHEMA] // Shallow Unit-Test
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should have as title `Book Rating`', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app.title).toBe(`Book Rating`);
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const nativeElement = fixture.nativeElement as HTMLElement;
+    expect(nativeElement.querySelector('h1')?.textContent).toContain('Book Rating')
   });
 });
