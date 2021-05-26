fd7cfd7509231d89b00061139512c74e16eb17c1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-26 09:14:40 +0200

    adding books

--- book-rating/libs/feature-books/src/lib/dashboard/dashboard.component.ts
+++ book-rating/libs/feature-books/src/lib/dashboard/dashboard.component.ts
@@ -50,4 +50,8 @@ export class DashboardComponent {
       .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
       .sort((a , b) => b.rating - a.rating)
   }
+
+  addBook(newBook: Book): void {
+    this.books = [...this.books, newBook];
+  }
 }
