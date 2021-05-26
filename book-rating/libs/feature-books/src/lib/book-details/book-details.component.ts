8429156983e3b63573891bd707b0b82e71d20cb7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-26 13:35:21 +0200

    poor man routing: using snapshot

--- book-rating/libs/feature-books/src/lib/book-details/book-details.component.ts
+++ book-rating/libs/feature-books/src/lib/book-details/book-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'books-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent  {
 
-  constructor() { }
+  isbn = '';
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.isbn = route.snapshot.paramMap.get('isbn') || '';
   }
 
 }
