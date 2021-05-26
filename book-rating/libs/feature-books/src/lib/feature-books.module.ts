4b799e64b4001783d578dce3a59dc1674047a9c8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-26 11:50:33 +0200

    routing: eager loading

--- book-rating/libs/feature-books/src/lib/feature-books.module.ts
+++ book-rating/libs/feature-books/src/lib/feature-books.module.ts
@@ -6,16 +6,20 @@ import { BookComponent } from './book/book.component';
 import { DataBooksModule } from '@book-rating/data-books';
 import { CreateBookComponent } from './create-book/create-book.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
-export const featureBooksRoutes: Route[] = [];
+const featureBooksRoutes: Route[] = [
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent }
+];
 
 @NgModule({
-  declarations: [DashboardComponent, BookComponent, CreateBookComponent],
+  declarations: [DashboardComponent, BookComponent, CreateBookComponent, BookDetailsComponent],
   imports: [
     CommonModule,
-    RouterModule,
+    RouterModule.forChild(featureBooksRoutes),
     DataBooksModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [DashboardComponent],
   // schemas: [CUSTOM_ELEMENTS_SCHEMA] // <-- deaktiviert Fehlerprüfung bei unbekannten Elementen
