ab440172a53aa746fea789a8114567162ef1c31a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-27 11:17:43 +0200

    rxjs: async pipe for the win!

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,57 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { Subject, ReplaySubject, timer, Subscription } from 'rxjs';
 import { takeWhile, takeUntil, take } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-unsubscribe',
   templateUrl: './unsubscribe.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnsubscribeComponent implements OnInit, OnDestroy {
+export class UnsubscribeComponent {
 
-  logStream$ = new ReplaySubject<string | number>();
-  // destroyed = false;
-  destroy$ = new Subject();
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen...
-   */
-  ngOnInit() {
-    const interval$ = timer(0, 1000);
-
-    interval$.pipe(
-
-      /******************************/
-
-      // takeWhile(() => !this.destroyed)
-      // takeUntil(this.destroy$)
-      take(5)
-
-      /******************************/
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-
-  }
-
-  ngOnDestroy() {
-    // this.destroyed = true;
-    this.destroy$.next();
-    this.logStream$.next('DESTROY');
-  }
+  zahl$ = timer(0, 1000);
 
 
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
 }
